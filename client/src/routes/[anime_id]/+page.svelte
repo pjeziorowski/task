0f208a163d9b98536e5684084494cd7f3fbd9e42
Patryk Jeziorowski <patryk.jeziorowski@gmx.com>
2024-03-13 11:37:07 +0100

    Add remove from favorites button

--- client/src/routes/[anime_id]/+page.svelte
+++ client/src/routes/[anime_id]/+page.svelte
@@ -1,32 +1,53 @@
 <script lang="ts">
-    import { enhance } from "$app/forms";
+    import { superForm } from "sveltekit-superforms/client";
     import Anime from "../anime.svelte";
     import type { PageData } from "./$types";
 
     export let data: PageData;
+
+    const addForm = superForm(data.addToFavoritesForm);
+    const { form: addToFavoritesForm, enhance: addToFavoritesEnhance } =
+        addForm;
+
+    const removeForm = superForm(data.removeFromFavoritesForm);
+    const {
+        form: removeFromFavoritesForm,
+        enhance: removeFromFavoritesEnhance,
+    } = removeForm;
 </script>
 
 <Anime
+animeId={data.anime.mal_id}
     title={data.anime.title}
-    mal_id={data.anime.mal_id}
     image={data.anime.images.webp.image_url}
 />
 
 <p>{data.anime.synopsis}</p>
 
-<form action="?/addToFavorites" method="post" use:enhance>
-    <input type="hidden" name="mal_id" value={data.anime.mal_id} />
-    <input type="hidden" name="title" value={data.anime.title} />
-    <input
-        type="hidden"
-        name="image"
-        value={data.anime.images.webp.image_url}
-    />
-    <button class="bg-gray-300 rounded p-4 mt-4" type="submit">
+<form action="?/addToFavorites" method="post" use:addToFavoritesEnhance>
+    <input type="hidden" name="animeId" value={$addToFavoritesForm.animeId} />
+    <input type="hidden" name="title" value={$addToFavoritesForm.title} />
+    <input type="hidden" name="image" value={$addToFavoritesForm.image} />
+    <button class="mt-4 rounded bg-gray-300 p-4" type="submit">
         Add to favorites
     </button>
 </form>
 
+<form
+    action="?/removeFromFavorites"
+    method="post"
+    use:removeFromFavoritesEnhance
+>
+    <input
+        type="hidden"
+        name="animeId"
+        value={$removeFromFavoritesForm.animeId}
+    />
+    <button class="mt-4 rounded bg-gray-300 p-4" type="submit">
+        Remove from favorites
+    </button>
+</form>
+
 <div class="mt-10">
-    <a class="bg-gray-300 rounded p-4" href="/">Go back to list</a>
+    <a class="rounded bg-gray-300 p-4" href="/">Go back to list</a>
 </div>
